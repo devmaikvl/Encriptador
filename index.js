e304b8be3a9ff02455ac1da4a312613eeb217828
devmaikvl <devmaikvl@gmail.com>
2024-07-25 02:44:03 -0600

    Se programan estilos y agrega función encriptar

---
+++ index.js
@@ -0,0 +1,28 @@
+function encriptar(){
+    let texto = document.getElementById("texto").value;
+    let titulomensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let textoCifrado = texto
+        
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+
+    if (document.getElementById("texto").value.length != 0){
+        document.getElementById("texto").value = textoCifrado;
+        document.getElementById("titulo-mensaje").textContent = "Texto encriptado con exito";
+        document.getElementById("parrafo").textContent = "";
+
+    } else {
+        alert("Debes ingresar un texto");
+        document.getElementById("titulo-mensaje").textContent = "Ningun mensaje fue encontrado";
+        document.getElementById("parrafo").textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+
+    }
+
+
+
+
+}
