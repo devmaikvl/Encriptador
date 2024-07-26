c02ce8ffe3af84276825bcbf880caca00af24703
devmaikvl <devmaikvl@gmail.com>
2024-07-26 12:06:32 -0600

    Proyecto finalizado.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function encriptar(){
     let texto = document.getElementById("texto").value;
+    const mayusculas = /[A-Z]/;
+    const acentos = /[áéíóúÁÉÍÓÚüÜñÑ]/;
+    
     let textoCifrado = texto
+    
         
         .replace(/e/gi, "enter")
         .replace(/i/gi, "imes")
@@ -8,15 +12,15 @@ function encriptar(){
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat");
 
-    if (document.getElementById("texto").value.length != 0){
+    if (document.getElementById("texto").value.length == 0 || mayusculas.test(texto) || acentos.test(texto)) {
+        alert("Debes ingresar un texto en minusculas y sin acentos");
+        document.getElementById("titulo-mensaje").textContent = "Ningun mensaje fue encontrado";
+        document.getElementById("parrafo").textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+
+    } else {
         document.getElementById("texto").value = textoCifrado;
         document.getElementById("titulo-mensaje").textContent = "Texto encriptado con exito";
         document.getElementById("parrafo").textContent = "";
-
-    } else {
-        alert("Debes ingresar un texto");
-        document.getElementById("titulo-mensaje").textContent = "Ningun mensaje fue encontrado";
-        document.getElementById("parrafo").textContent = "Ingresa el texto que deseas encriptar o desencriptar";
 
     }
 
@@ -27,6 +31,9 @@ function encriptar(){
 
 function desencriptar(){
     let texto = document.getElementById("texto").value;
+    const mayusculas = /[A-Z]/;
+    const acentos = /[áéíóúÁÉÍÓÚüÜñÑ]/;
+    
     let textoDesencriptado = texto
         
         .replace(/enter/gi, "e")
@@ -35,14 +42,15 @@ function desencriptar(){
         .replace(/ober/gi, "o")
         .replace(/ufat/gi, "u");
     
-    if (document.getElementById("texto").value.length != 0){
-        document.getElementById("texto").value = textoDesencriptado;
-        document.getElementById("titulo-mensaje").textContent = "Texto desencriptado con exito";
+    if (document.getElementById("texto").value.length == 0 || mayusculas.test(texto) || acentos.test(texto)){
+        alert("Debes ingresar un texto en minusculas y sin acentos");
+        document.getElementById("titulo-mensaje").textContent = "Ningun mensaje fue encontrado";
         document.getElementById("parrafo").textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         
+        
     } else {
-        alert("Debes ingresar un texto");
-        document.getElementById("titulo-mensaje").textContent = "Ningun mensaje fue encontrado";
+        document.getElementById("texto").value = textoDesencriptado;
+        document.getElementById("titulo-mensaje").textContent = "Texto desencriptado con exito";
         document.getElementById("parrafo").textContent = "Ingresa el texto que deseas encriptar o desencriptar";
     }
 }
